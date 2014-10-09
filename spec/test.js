6d7d0e2cc2c0694def4d03ad6cad36a6ac6d529b
codemeup <laurakemp@live.co.uk>
2014-10-08 20:11:32 -0700

    lab done

--- spec/test.js
+++ spec/test.js
@@ -7,7 +7,7 @@ expect = require('chai').expect;
 describe('Car', function(){
 
   beforeEach(function(){
-    // create a new myCar object each time
+    car1 = new Car("Aston", "DB9", 2004, "blue");
   });
 
   describe('#year', function(){
@@ -44,56 +44,84 @@ describe('Car', function(){
   describe('#sale', function(){
 
     it('should move currentOwner to previousOwners array', function(){
-
+      car1.sale("Peter");  
+      expect(car1.previousOwners).to.eql(["manufacturer"]);
     });
 
     it('should update currentOwner with the new owner', function(){
-
+      car1.sale("Peter");  
+      expect(car1.currentOwner).to.equal("Peter");
     });
   });
 
   describe('#paint', function(){
     it('should update the color of myCar', function(){
-
+    car1.paint("red");
+    expect(car1.color).to.equal("red");
     });
   });
 
   describe('#start', function(){
-    it('should update the state to on', function(){
-
+    it('should update the carRunning to on', function(){
+      car1.start();
+      expect(car1.carRunning).to.equal(true);
     });
   });
 
   describe('#off', function(){
-    it('should update the state to off', function(){
-
+    it('should update the carRunning to off', function(){
+      car1.off();
+      expect(car1.carRunning).to.equal(false);
     });
+  });
+
+  describe('#driveTo', function(){
+    it('should return "Driving to Cuba." only when the carRunning is true', function(){
+      car1.carRunning = true;
+      expect(car1.driveTo("Cuba")).to.equal("Driving to Cuba.");
+    });
+
   });
 
   describe('#park', function(){
     it('should make sure to only work when the car is off', function(){
-
+      car1.carRunning = false;
+      expect(car1.park()).to.equal("Parked!!");
+      //  });
+      // it('should make sure to only work when the car is off', function(){
+      // car1.carRunning = true;
+      // expect(carRunning.park).to.equal("");
     });
 
   });
 
   describe('#pickUp', function(){
     it('should add the passenger to the passengers array if car is on', function(){
-
+      car1.carRunning = true;
+      car1.pickUp("Peter");  
+      expect(car1.passengers).to.eql(["Peter"]);
     });
 
     it('should not modify the passengers array if car is off', function(){
-
+      car1.carRunning = false;
+      car1.pickUp("Peter");  
+      expect(car1.passengers).to.eql([]);
     });
   });
 
   describe('#dropOff', function(){
     it('should remove passenger from the passengers array if car is on', function(){
-
+      car1.carRunning = true;
+      car1.passengers = ["Peter"];
+      car1.dropOff("Peter");
+      expect(car1.passengers).to.eql([]);
     });
 
     it('should leave passenger in the passengers array if car is off', function(){
-
+      car1.carRunning = false;
+      car1.passengers = ["Peter"];
+      car1.dropOff("Peter");
+      expect(car1.passengers).to.eql(["Peter"]);
     });
   });
 
--- src/car.js
+++ src/car.js
@@ -1,13 +1,65 @@
-function Car(make, model, year, color){
 
+
+function Car(make, model, year, color){
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.currentOwner = "manufacturer";
+  this.previousOwners = [];
+  this.carRunning = false;
+  this.passengers = [];
 }
 
 Car.prototype.sale = function(newOwner){
-
+	this.previousOwners.push(this.currentOwner);
+	this.currentOwner = newOwner;
 };
 
 Car.prototype.paint = function(newColor){
-
+	this.color = newColor;
 };
 
-module.exports=Car;
+Car.prototype.state = function(changeState){
+  this.carRunning = !this.carRunning;
+};
+
+Car.prototype.start = function(){
+  this.carRunning = true;
+};
+
+Car.prototype.off = function(){
+  this.carRunning = false;
+};
+
+Car.prototype.driveTo = function(destination){
+  if(this.carRunning === true);
+  var driveToString = ("Driving to " + destination + ".");
+  console.log(driveToString);
+  return(driveToString);
+};
+
+
+Car.prototype.park = function(){
+  if(this.carRunning === false);
+  var parkedString = ("Parked!!");
+  console.log(parkedString);
+  return(parkedString);
+};
+
+Car.prototype.pickUp = function(name){
+  if(this.carRunning === true){
+  console.log("Driving to pick up " + name);
+  this.passengers.push(name);
+  }
+};
+
+Car.prototype.dropOff = function(name){
+  if(this.carRunning === true && this.passengers.indexOf(name) !== -1){
+  console.log("Dropping off " + name);
+  // this.passengers.replace(name, "");
+  this.passengers.splice(this.passengers.indexOf(name), 1);
+  }
+};
+
+module.exports = Car;
